fc6e27f3d573ec634b2651c8d7abb1f35f675522
GabrielBR90 <139515580+GabrielBR90@users.noreply.github.com>
2025-02-09 21:13:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("btnClick").addEventListener("click", function() {
-    alert("Você clicou no botão!");
+    alert("Você clicou no botão da versão desktop!");
+});
+
+document.getElementById("mobileBtn").addEventListener("click", function() {
+    alert("Você clicou no botão da versão mobile!");
 });
